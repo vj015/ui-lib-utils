71c127a0b7d7ad75f0bd5b2172f667c24cf1c9a3
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-12-26 02:49:08 +0530

    basic template and style for component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <h1>{{ "App" }}</h1>
+    <QRGenerator />
   </div>
 </template>
 
 <script>
+import QRGenerator from "./components/QRGenerator.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    QRGenerator,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
