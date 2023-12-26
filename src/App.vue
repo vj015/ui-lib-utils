558f0d5bb09f2d9305d844f5292e4a96eceaa2d7
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-12-26 19:02:15 +0530

    Added another component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <h1>{{ "App" }}</h1>
     <QRGenerator />
+    <MyButton />
   </div>
 </template>
 
 <script>
 import QRGenerator from "./components/QRGenerator.vue";
+import MyButton from "./components/MyButton.vue";
 export default {
   name: "App",
   components: {
     QRGenerator,
+    MyButton,
   },
   data() {
     return {};
